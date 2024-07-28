82f53fba98b90577ef42e6e9edac758f4d4d4c02
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-07-27 23:22:20 -0700

    Some

--- index.js
+++ index.js
@@ -4,41 +4,73 @@ const liftFloor = document.querySelectorAll(".floor");
 const lift = document.querySelector(".lift");
 const liftLeftDoor = document.querySelector(".lift-left-door");
 const liftRightDoor = document.querySelector(".lift-right-door");
-liftBtn.forEach((e) => {
-  e.addEventListener("click", (t) => {
-    liftFloor.forEach((e) => {
-      if (t.currentTarget.textContent === e.textContent) {
+
+const liftOpenDoor = () => {
+  gsap.to(liftLeftDoor, {
+    x: "-100%",
+    duration: 1,
+    ease: Power1.easeInOut,
+  });
+  gsap.to(liftRightDoor, {
+    x: "100%",
+    duration: 1,
+    ease: Power1.easeInOut,
+  });
+};
+
+const liftCloseDoor = () => {
+  gsap.to(liftLeftDoor, {
+    x: 0,
+    duration: 1,
+    ease: Power1.easeInOut,
+  });
+  gsap.to(liftRightDoor, {
+    x: 0,
+    duration: 1,
+    ease: Power1.easeInOut,
+  });
+};
+
+const liftBtnVisible = () => {
+  {
+    liftBtn.forEach((e) => {
+      gsap.to(e, {
+        cursor: "pointer",
+        opacity: 1,
+        zIndex: 1,
+      });
+    });
+  }
+};
+
+const liftBtnUnVisible = () => {
+  {
+    liftBtn.forEach((e) => {
+      gsap.to(e, {
+        cursor: "default",
+        opacity: 0.5,
+        zIndex: -1,
+      });
+    });
+  }
+};
+
+liftBtn.forEach((liftBtnElem) => {
+  liftBtnElem.addEventListener("click", (t) => {
+    liftFloor.forEach((liftFloorElem) => {
+      if (t.currentTarget.textContent === liftFloorElem.textContent) {
         gsap.to(lift, {
-          top: e.getBoundingClientRect().top + "px",
-          bottom: e.getBoundingClientRect().bottom + "px",
-          duration: 1,
-          ease: Power2.easeInOut,
+          top: liftFloorElem.getBoundingClientRect().top + "px",
+          bottom: liftFloorElem.getBoundingClientRect().bottom + "px",
+          duration: 3,
+          ease: Power3.easeInOut,
+          onComplete: () => {
+            setTimeout(liftOpenDoor, 0);
+            setTimeout(liftCloseDoor, 2000);
+            setTimeout(liftBtnVisible, 2500);
+          },
         });
-        let liftOpenDoor = setInterval(() => {
-          gsap.to(liftLeftDoor, {
-            x: "-100%",
-            duration: 1,
-            ease: Power2.easeInOut,
-          });
-          gsap.to(liftRightDoor, {
-            x: "100%",
-            duration: 1,
-            ease: Power2.easeInOut,
-          });
-          setInterval(() => {
-            gsap.to(liftLeftDoor, {
-              x: 0,
-              duration: 1,
-              ease: Power2.easeInOut,
-            });
-            gsap.to(liftRightDoor, {
-              x: 0,
-              duration: 1,
-              ease: Power2.easeInOut,
-            });
-          }, 2000);
-          clearInterval(liftOpenDoor);
-        }, 1000);
+        liftBtnUnVisible();
       }
     });
   });
