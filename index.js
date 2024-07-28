9cb3bcc9e74bc2a8c75d8b38663fd6ef2566159f
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-07-27 21:47:26 -0700

    Some

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const liftFloors = document.querySelector(".lift-floors");
 const liftBtn = document.querySelectorAll(".lift-btn");
 const liftFloor = document.querySelectorAll(".floor");
 const lift = document.querySelector(".lift");
+const liftLeftDoor = document.querySelector(".lift-left-door");
+const liftRightDoor = document.querySelector(".lift-right-door");
 liftBtn.forEach((e) => {
   e.addEventListener("click", (t) => {
     liftFloor.forEach((e) => {
@@ -9,9 +11,34 @@ liftBtn.forEach((e) => {
         gsap.to(lift, {
           top: e.getBoundingClientRect().top + "px",
           bottom: e.getBoundingClientRect().bottom + "px",
-          duration: 4,
+          duration: 1,
           ease: Power2.easeInOut,
         });
+        let liftOpenDoor = setInterval(() => {
+          gsap.to(liftLeftDoor, {
+            x: "-100%",
+            duration: 1,
+            ease: Power2.easeInOut,
+          });
+          gsap.to(liftRightDoor, {
+            x: "100%",
+            duration: 1,
+            ease: Power2.easeInOut,
+          });
+          setInterval(() => {
+            gsap.to(liftLeftDoor, {
+              x: 0,
+              duration: 1,
+              ease: Power2.easeInOut,
+            });
+            gsap.to(liftRightDoor, {
+              x: 0,
+              duration: 1,
+              ease: Power2.easeInOut,
+            });
+          }, 2000);
+          clearInterval(liftOpenDoor);
+        }, 1000);
       }
     });
   });
